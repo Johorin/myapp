fc5645456a5548b6c1758b67c2b8a43f740f93a1
Johorin <hoyuak2145@docomo.ne.jp>
2022-03-15 19:45:49 +0900

    背景画像が表示されるように＆createビューの調整１

--- resources/views/create/create.blade.php
+++ resources/views/create/create.blade.php
@@ -6,16 +6,38 @@
     <head>
         <meta charset="utf-8">
         <title>SELF-SUFFICIENT BLOG</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Reset CSS -->
+        <link href="{{asset('/css/reset.css')}}" rel="stylesheet">
+        <!-- CSSの読み込み -->
+        <link href="{{asset('/css/create.css')}}" rel="stylesheet">
     </head>
     <body>
         <form action="/posts" method="POST">
             @csrf
-            <div class="body">
-                <textarea name="post[body]" placeholder="投稿する内容を入力"></textarea>
+            <div class="post_example">
+                <div class="post_example__profileImage_and_userName">
+                    <div class="post_example__image_and_userName--profileImage">
+                        <p>プロフィール画像</p>
+                    </div>
+                    <div class="post_example__image_and_userName--userName">
+                        <p>{{Auth::user()->name}}</p>
+                    </div>
+                </div>
+                <div class="post_example__body">
+                    <textarea name="post[body]" placeholder="投稿する内容を入力"></textarea>
+                </div>
             </div>
-            <div class="back">[<a href="/">back</a>]</div>
-            <input type="submit" value="POST"/>
-            <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+            <div class="botton">
+                <div class="botton__back">
+                    <a href="/">⬅︎ BACK</a>
+                </div>
+                <div class="botton__post">
+                    <input type="submit" value="POST"/>
+                    <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
+                </div>
+            </div>
         </form>
     </body>
 </html>
